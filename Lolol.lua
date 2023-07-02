c72c647e13e244e8087d5455c856ec690b0ee03c
Ambatukam <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-07-02 15:41:23 +0900

    Update Lolol.lua

--- Lolol.lua
+++ Lolol.lua
@@ -11,11 +11,10 @@ local Tab1 = Window_1:NewSection("Main")
 getgenv().BallDirection = Vector3.new(999999, 999999, 999999)
 
 local plr = game.Players.LocalPlayer
-local ball = workspace.Ball
+local ball = nil
 
 function getBall()
 while true do 
-    game:GetService("RunService").Stepped:Wait()
     if workspace:FindFirstChild("Ball") ~= nil then
        ball = workspace.Ball
     end
@@ -25,19 +24,20 @@ end
 task.spawn(getBall)
 
 Tab1:CreateToggle("Auto Deflect", function(value)
-_G.Flect = value
+_G.Y = value
 while wait() do
-      if _G.Flect == false then break end
+      if _G.Y == false then break end
     game:GetService("RunService").Stepped:Wait()
      if (ball and ball:FindFirstChild("Main") and plr.Character ~= nil) then
         local distance = plr:DistanceFromCharacter(ball.Main.Position)
+	plr.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.lookAt(plr.Character.HumanoidRootPart.Position, Vector3.new(ball.Main.Position.X, plr.Character.HumanoidRootPart.Position.Y, ball.Main.Position.Z))
           if (distance <= 17.8) then
             plr.Character:WaitForChild("Deflection").Remote:FireServer("Deflect", BallDirection)
           end
        end
     end
 end)
-
+--[[
 Tab1:CreateToggle("Aimbot", function(value)
 _G.Aimbot = value
     while wait() do
@@ -45,3 +45,4 @@ _G.Aimbot = value
          plr.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.lookAt(plr.Character.HumanoidRootPart.Position, Vector3.new(ball.Main.Position.X, plr.Character.HumanoidRootPart.Position.Y, ball.Main.Position.Z))
     end
 end)
+]]
